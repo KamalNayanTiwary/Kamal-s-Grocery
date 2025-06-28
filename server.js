03ef047929a0694acf7fdbf7cb23f5d20ccf23d4
Kamal Nayan Tiwary <kamalnayantiwary73@gmail.com>
2025-06-28 06:12:10 +0530

    FIX

--- server.js
+++ server.js
@@ -37,8 +37,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
+
 
 // === Session Setup (updated for cross-origin)
 app.use(session({
